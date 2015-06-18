9159f195d13c435a0fcbb91028e48b3d60ffddc4
Hope Soft <hope.soft.bd@gmail.com>
2015-06-18 10:56:23 +0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -46,6 +46,7 @@
         <div id="map_canvas"></div>
     </body>
     </html>
+    
 <?php
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
